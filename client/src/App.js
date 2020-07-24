bcffaf6f28693ea9a8b481a7f323f1af7373e92d
Your Name <j.hamwijk34@gmail.com>
2020-07-24 08:33:38 +0200

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,27 @@
 import './App.css';
-import React from "react";
+import React  from "react";
+
+//Material UI style button
+import {Button, Grid} from "@material-ui/core";
+import HiddenCss from "@material-ui/core/Hidden/HiddenCss";
+
 
 function App() {
-  return (
-    <div className="App">
-      <h1>getting started!</h1>
 
 
-    </div>
-  );
+    return (
+        <Grid>
+            <div className="App">
+                <div>
+                    <h1>Administrative reactive</h1>
+                    <p>1.0</p>
+
+                    <Button type="submit" variant="contained">Erwins Enter</Button>
+                </div>
+
+            </div>
+        </Grid>
+    );
 }
 
 export default App;
